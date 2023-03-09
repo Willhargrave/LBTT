4a5c540434eeaf77c9bed455087565471869aee3
will <willhargrave@hotmail.co.uk>
2023-03-09 10:11:52 +0900

    added rates

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-// const stampDutyCalc = (price) => {
-//     let stampdutyObject = {
-//       ftb: 0,
-//       rtb: 0,
-//       shb: 0,
-//     };
-  
-//     firstTimeBuyerCalc(price, stampdutyObject);
-//     returningBuyerCalc(price, stampdutyObject);
-//     secondHomeBuyerCalc(price, stampdutyObject);
-  
-//     return stampdutyObject;
-//   };
 let bandA = 145000;
 let bandB = 250000;
 let bandC = 325000;
-let bandD = bandD;
+let bandD = 750000;
 
+let rateA = 0.02;
+let rateB = 0.05;
+let rateC = 0.1;
+let rateD = 0.12;
   function calculate(price) {
     if (price < bandA) {
       return 0;
     } else if (price > bandA && price <= bandB) {
-      return price * 0.02;
+      return price * rateA;
     } else if (price > bandB && price <= bandC) {
-      return (price - bandB) * 0.05 + (bandB - bandA) * 0.02;
+      return (price - bandB) * rateB + (bandB - bandA) * rateA;
     } else if (price > bandC && price <= bandD) {
-      return (price - bandC) * 0.1 + (bandC - bandB) * 0.05 + (bandB - bandA) * 0.02;
+      return (price - bandC) * rateC + (bandC - bandB) * rateB + (bandB - bandA) * rateA;
     } else {
-      return (price - bandD) * 0.12 + (bandD - bandC) * 0.1 + (bandC - bandB) * 0.05 + (bandB - bandA) * 0.02;
+      return (price - bandD) * rateD + (bandD - bandC) * rateC + (bandC - bandB) * rateB + (bandB - bandA) * rateA;
     }
   }
   
